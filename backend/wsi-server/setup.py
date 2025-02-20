aaa63591ad524d9c6270d8a56a3b8abf839545d0
David Morrison <dm236@st-andrews.ac.uk>
2025-02-20 14:59:19 +0000

    add in preprocessor

---
+++ backend/wsi-server/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Preprocessor',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Preprocessor for Histopathology.',
+    author='David Morrison'
+)
